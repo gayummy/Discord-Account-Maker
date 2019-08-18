5a860a9e7ebb85137aa40a80e7866ac916372b3e
gayummy <46387835+gayummy@users.noreply.github.com>
2019-08-18 00:01:47 -0600

    Create accountcreator.py

---
+++ accountcreator.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import sys
+import os
+from time import sleep
+sys.path.append ("././.")
+from config import *
+
+account_Email = sys.argv[1]
+account_Password sys.argv[2]
+PROXY = sys.argv[3]
+API_KEY = captchaAPI # Your 2captcha API KEY
+site_key = '6Lef5iQTAAAAAKeIvIY-DeexoO3gj7ryl9rLMEnn'
+url = "https://discordapp.com/api/v6/auth/register"
+
+### NOT DONE YET ;( (ill work on this later afire and joe ;) ) ###
